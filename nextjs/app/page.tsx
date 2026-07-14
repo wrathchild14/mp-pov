cbcf61cb91c3d8892de44f3e28b35845a09c7485
wrathchild14 <jovanprodanov123@gmail.com>
2026-07-14 22:06:42 +0200

    add something else for project

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Photographer Portfolio</h1>
+      <p>Welcome to my work.</p>
+
+      <a href="/cloud" style={{ color: "blue" }}>
+        Client Delivery Portal
+      </a>
+    </main>
+  );
+}
